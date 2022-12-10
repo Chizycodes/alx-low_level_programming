ff23243a7c5a13e30fc10a07aad03d460f23943e
chizycodes <okwudirejoy@gmail.com>
2022-12-09 22:30:55 -0800

    stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
  *
- * Description: 'unix'
+ * Description: 'Write'
  *
  * Return: Always 1 (Success)
 */
 
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
