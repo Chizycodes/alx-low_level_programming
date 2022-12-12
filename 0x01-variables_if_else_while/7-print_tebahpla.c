4340222c775f96f532f673cd57af2b9f41f45457
chizycodes <okwudirejoy@gmail.com>
2022-12-12 09:04:32 -0800

    -

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints numbers z to a'
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
