20d10f35b6a52dfbd562725dbf12f091e0aa9786
chizycodes <okwudirejoy@gmail.com>
2022-12-12 19:54:21 -0800

    _

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints numbers'
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			putchar((x % 10) + '0');
+			putchar((y % 10) + '0');
+
+			if (x == 8 && y == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
