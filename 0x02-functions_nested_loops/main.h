401541ede18adf576b49bf339ab2106d666d0ea8
chizycodes <okwudirejoy@gmail.com>
2022-12-13 18:50:20 -0800

    -

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ *
+ * Description: Header file containing function declarations used in this directory.
+ *
+*/
+
+int _putchar(char c);
+
+#endif
