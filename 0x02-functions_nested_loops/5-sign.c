06a7c51544cc1d8c801af74db56b1a39a9c1490d
chizycodes <okwudirejoy@gmail.com>
2022-12-13 20:50:32 -0800

    -

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _isalpha - Prints the sign of a number.
+ * @n: The character to be checked.
+ *
+ * Return: 1 if n is greater than zero,
+ *	0 if n is zero,
+ *	-1 if n is less than zero.
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
