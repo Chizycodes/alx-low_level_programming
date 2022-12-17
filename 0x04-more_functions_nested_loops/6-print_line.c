32f70d244294ed6f9308830b0c97f50d95c971fc
chizycodes <okwudirejoy@gmail.com>
2022-12-16 22:35:01 -0800

    -

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,6 +2,7 @@
 
 /**
  * print_line - Draws a straight line in the terminal.
+ * @n: number of _ to be printed.
  */
 
 void print_line(int n)
@@ -11,9 +12,7 @@ void print_line(int n)
 	if (n > 0)
 	{
 		for (num = 0; num < n; num++)
-		{
 			_putchar('_');
-		}
 	}
 
 	_putchar('\n');
