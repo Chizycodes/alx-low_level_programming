be319174d2b8f629bb762b4729c5eea976db7541
chizycodes <okwudirejoy@gmail.com>
2022-12-21 11:31:46 -0800

    -

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string.
+ * @str: string.
+ */
+
+void puts2(char *str)
+{
+	int len = 0, index = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+	{
+		_putchar(str[index]);
+	}
+
+	_putchar('\n');
+}
