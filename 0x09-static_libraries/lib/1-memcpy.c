2e5cea351e5adebb54ec95a05efb03c219cfcf9d
chizycodes <okwudirejoy@gmail.com>
2023-01-07 18:12:27 -0800

    static libraries

---
+++ 0x09-static_libraries/lib/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from the memory
+ *	area of src to the memory area desc.
+ * @src: pointer to the memory to be copied.
+ * @dest: the pointer to the memory area to copy to.
+ * @n: the number of bytes to be copied.
+ *
+ * Return: A pointer to desc.
+ */
+
+void *_memcpy(void *dest, char *src, unsigned int n)
+{
+	unsigned int index;
+	unsigned char *des = dest;
+	char *source = src;
+
+	for (index = 0; index < n; index++)
+		des[index] = source[index];
+
+	return (dest);
+}
---
+++ 0x09-static_libraries/lib/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Compute the absolute value of an integer.
+ * @n: The character to be checked.
+ *
+ * Return: The absolute value of n.
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
