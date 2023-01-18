dbbfe3d8edb99a26ccd9dcdc4e15f0275b74dd91
chizycodes <okwudirejoy@gmail.com>
2023-01-17 17:46:51 -0800

    typeof

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,20 +1,30 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * init_dog - initializes a variable of type struct dog.
+ * print_dog - prints a struct dog.
  * @d: the dog.
- * @name: the age of the dog.
- * @age: theage of the dog.
- * @owner: the owner.
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void print_dog(struct dog *d)
 {
 	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+
 }
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog -creates a new dog.
+ * @name: name.
+ * @age: age.
+ * @owner: owner.
+ *
+ * Return: new dog;
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *bingo;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	bingo = malloc(sizeof(dog_t));
+
+	if (bingo == NULL)
+		return (NULL);
+
+	bingo->name = name;
+	bingo->age = age;
+	bingo->owner = owner;
+
+	return (bingo);
+	
+}
