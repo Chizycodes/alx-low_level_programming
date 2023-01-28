46d726a1c87bd2cbc105d86f3f38738159f3c2fc
chizycodes <okwudirejoy@gmail.com>
2023-01-28 13:35:57 -0800

    print number of elements in a linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,25 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_list - prints all the elements of a list_t list
+ * list_len - returns number of element in a linked list_t list.
  * @h: pointer to the list_t list
  *
- * Return: the number of nodes
+ * Return: the number of elements.
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-    size_t nodes = 0;
+	size_t elem = 0;
 
-    while (h)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
+	while (h)
+	{
+		elem += 1;
 
-        nodes++;
-        h = h->next;
-    }
+		h = h->next;
+	}
 
-    return (nodes);
+	return (elem);
 }
