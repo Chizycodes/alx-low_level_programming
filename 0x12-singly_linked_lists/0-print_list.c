53ed613f56a76b4c0e8fc2525bac119499978b52
chizycodes <okwudirejoy@gmail.com>
2023-01-28 13:09:19 -0800

    fix betty issues

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;;
+	size_t nodes = 0;
 
 	while (h)
 	{
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		
+
 		nodes++;
 		h = h->next;
 	}
